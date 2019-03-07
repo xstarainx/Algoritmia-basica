6d3012498eb20003ddc85b57e189cb44e5803b7f
xstarainx <32374903+xstarainx@users.noreply.github.com>
2019-03-07 10:32:32 +0100

    first commit

---
+++ prac1/arbol.h
@@ -0,0 +1,10 @@
+#ifndef ARBOL_H
+#define ARBOL_H
+
+#include <iostream>
+using namespace std;
+
+
+
+
+#endif
---
+++ prac1/prueba.cpp
@@ -0,0 +1,55 @@
+/*
+ * Fichero de prueba
+ */
+
+#include <iostream>
+#include <stdlib.h>
+#include <fstream>
+#include <map>
+
+const int MAX_CHARS = 300;
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	//Si el numero de parametros es incorrecto indicar y finalizar
+	if (argc != 2) {
+		cout << "Numero de parametros incorrecto" << endl;
+		exit(0);
+	}
+
+	string nom_fich = argv[1];
+
+	ifstream f;		//Flujo de entrada
+	ofstream f2;	//Flujo de salida
+
+	f.open(argv[1]);
+
+	if (f.is_open()) {
+		cout << "Abierto correctamente" << endl;
+
+		char c;
+		map<char,int> tabla;
+		//Recorrido del fichero contando apariciones
+		while(!f.eof()) {
+			c = f.get();
+			tabla[c] += 1;
+		}
+
+		
+		//Iterador que muestra la tabla de frecuencias
+		map<char, int>::iterator it;
+		for ( it = tabla.begin(); it != tabla.end(); it++) {
+    		std::cout << it->first << ':'<< it->second << std::endl ;
+		}
+		
+
+
+		f.close();
+	}
+	else {
+		cout << "Error al abrir el fichero o nombre de fichero incorrecto" << nom_fich << endl;
+	}
+
+
+}
